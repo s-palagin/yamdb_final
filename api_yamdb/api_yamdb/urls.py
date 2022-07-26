406af39b090fa4703fa716728e84af1d793e8029
Sergey Palagin <s.palagin@gmail.com>
2022-07-26 20:35:31 +0300

    fixed workflows

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -8,6 +8,11 @@ api_patterns = [
 ]
 
 urlpatterns = [
+    path(
+        '/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
     path('admin/', admin.site.urls),
     path('api/', include(api_patterns)),
     path(
